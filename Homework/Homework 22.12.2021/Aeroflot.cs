a171f3843efaba84d60adaed0dadcfad154e6d5d
Алсу Фаизова <AIFaizova@stud.kpfu.ru>
2021-12-20 21:12:58 +0300

    /

---
+++ Homework/Homework 22.12.2021/Aeroflot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Homework_22._12._2021
+{
+    internal class Aeroflot
+    {
+
+    }
+}
