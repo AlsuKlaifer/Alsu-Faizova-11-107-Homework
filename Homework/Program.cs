24948ed3725f2964a09b903e04cf55f4f47b564b
Алсу Фаизова <AIFaizova@stud.kpfu.ru>
2021-12-20 19:56:06 +0300

    .

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,6 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Homework.Christmas;
 using Homework.Extension;
+using Homework.kontr3;
+
+//Kontr3
+var t1 = new KitchenTechnic("Холодильник", "Россия", 1500, "A", false);
+var t2 = new KitchenTechnic("Плита", "Франция", 500, "В", true);
+var t3 = new KitchenTechnic("Духовка", "Англия", 700, "A", true);
+var arr = new KitchenTechnic[] { t1, t2};
+var queue = new Queue(arr);
+queue.Add(t3);
+var queueClone = queue.Clone() as Queue;
+queueClone.FileWrite();
+Console.WriteLine(queue.Read()); 
 
 //ICloneable
 //var c1 = new ComplexNumber(1, -32);
@@ -18,17 +30,17 @@ using Homework.Extension;
 //Console.WriteLine(s.OneByOneJoin("купить съесть убрать"));
 
 //Christmas
-var g1 = new Garland(5, 2, 2, true);
-var g2 = new Garland(5, 2, 3, true);
-var g3 = new Garland(5, 2, 4, true);
-var garlands = new Garland[] { g1, g2, g3 };
-var t1 = new Toy(50, true, 1, false);
-var t2 = new Toy(10, true, 1, false);
-var t3 = new Toy(50, true, 2, false);
-var toys = new Toy[] { t1, t2, t3 };
+//var g1 = new Garland(5, 2, 2, true);
+//var g2 = new Garland(5, 2, 3, true);
+//var g3 = new Garland(5, 2, 4, true);
+//var garlands = new Garland[] { g1, g2, g3 };
+//var t1 = new Toy(50, true, 1, false);
+//var t2 = new Toy(10, true, 1, false);
+//var t3 = new Toy(50, true, 2, false);
+//var toys = new Toy[] { t1, t2, t3 };
 
-var tree = new ChristmasTree("Елка", 200, 6, 2);
-var showcase = new Showcase("Витрина", 10, 4);
+//var tree = new ChristmasTree("Елка", 200, 6, 2);
+//var showcase = new Showcase("Витрина", 10, 4);
 
-Decorate.Decorating(showcase, garlands, toys);
-Decorate.Decorating(tree, garlands, toys);
+//Decorate.Decorating(showcase, garlands, toys);
+//Decorate.Decorating(tree, garlands, toys);
---
+++ Homework/kontr3/Technic.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.kontr3
+{
+    public abstract class Technic
+    {
+        public readonly string Name;
+        public readonly string Country;
+        public int Power { get; }
+        public string EnergyClass { get; }
+
+        public Technic(string name, string country, int power, int energy)
+        {
+            Name = name;
+            Country = country;
+            Power = power;
+            EnergyClass = energy;
+        }
+        public override int GetHashCode()
+        {
+            int hashcode = Name.GetHashCode();
+            hashcode = 31 * hashcode + Country.GetHashCode();
+            return hashcode;
+        }
+        public override bool Equals(object t)
+        {
+            if (!(t is Technic))
+                throw new Exception("not a technic");
+            var a = t as Technic;
+            if (a.Name != Name || a.Country != Country || a.Power != Power || a.EnergyClass != EnergyClass)
+                return false;
+            else
+                return true;
+        }
+        public override string ToString()
+        {
+            return $"Название техники: {Name}" +
+                $"Страна производителя: {Country}" +
+                $"Мощность: {Power}" +
+                $"Класс Энергосбережения {EnergyClass}";
+        }
+}
